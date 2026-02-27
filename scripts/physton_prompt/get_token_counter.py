9b431a50b71da4ab04d6583e7419bdc22d1ac0b0
DestoryG <numbersans97@gmail.com>
2026-02-27 11:57:28 +0800

    Fix Forge Neo token counter and dependency install behavior

--- scripts/physton_prompt/get_token_counter.py
+++ scripts/physton_prompt/get_token_counter.py
@@ -1,5 +1,4 @@
 from modules import script_callbacks, extra_networks, prompt_parser, sd_models
-from modules.sd_hijack import model_hijack
 from functools import partial, reduce
 
 
@@ -12,30 +11,25 @@ def get_token_counter(text, steps):
             text, _ = extra_networks.parse_prompt(text)
 
             _, prompt_flat_list, _ = prompt_parser.get_multicond_prompt_list([text])
-            prompt_schedules = prompt_parser.get_learned_conditioning_prompt_schedules(prompt_flat_list, steps)
+            prompt_schedules = prompt_parser.get_learned_conditioning_prompt_schedules(
+                prompt_flat_list, steps
+            )
 
         except Exception:
             # a parsing error can happen here during typing, and we don't want to bother the user with
             # messages related to it in console
             prompt_schedules = [[[steps, text]]]
 
-        try:
-            from modules_forge import forge_version
-            forge = True
-
-        except:
-            forge = False
-
         flat_prompts = reduce(lambda list1, list2: list1 + list2, prompt_schedules)
         prompts = [prompt_text for step, prompt_text in flat_prompts]
 
-        if forge:
-            cond_stage_model = sd_models.model_data.sd_model.cond_stage_model
-            token_count, max_length = max([model_hijack.get_prompt_lengths(prompt,cond_stage_model) for prompt in prompts],
-                                        key=lambda args: args[0])
-        else:
-            token_count, max_length = max([model_hijack.get_prompt_lengths(prompt) for prompt in prompts],
-                                        key=lambda args: args[0])
+        get_prompt_lengths_on_ui = (
+            sd_models.model_data.sd_model.get_prompt_lengths_on_ui
+        )
+        token_count, max_length = max(
+            [get_prompt_lengths_on_ui(prompt) for prompt in prompts],
+            key=lambda args: args[0],
+        )
 
         return {"token_count": token_count, "max_length": max_length}
 
